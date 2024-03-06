1b8bb80adba3737041a2aa55f42fcb9061db30ff
Utkarsh Shukla <utkshukla9@gmail.com>
2024-03-06 17:41:13 +0530

    Configured and Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Database is connected....");
+}).catch((err) => {
+    console.log(`DB Connection Error is ${err}`);
+});
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is currently running on Port ${PORT} `);
 });
