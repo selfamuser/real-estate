db6bbe73ee58643c2362d1cb3abb1c052b7f9099
Utkarsh Shukla <utkshukla9@gmail.com>
2024-03-08 17:13:02 +0530

    Initialized Redux Toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state)=>state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -13,34 +19,29 @@ export default function SignIn() {
     });
   };
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      const res = await fetch("/api/auth/signin",{
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json',
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
+     // console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
-      navigate('/');
-
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
-
+      dispatch(signInFailure(error.message));
     }
-  }
+  };
 
   return (
     <>
@@ -63,8 +64,11 @@ export default function SignIn() {
             id="password"
             onChange={handleChange}
           />
-          <button disabled={loading} className="bg-purple-800 text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-60">
-            {loading ? 'Loading...' : "Sign In"}
+          <button
+            disabled={loading}
+            className="bg-purple-800 text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-60"
+          >
+            {loading ? "Loading..." : "Sign In"}
           </button>
         </form>
         <div className="mt-3 text-center">
@@ -78,7 +82,7 @@ export default function SignIn() {
             </Link>
           </p>
         </div>
-        {error && <p className='text-red-500 mt-5 text-center'>{error}</p>}
+        {error && <p className="text-red-500 mt-5 text-center">{error}</p>}
       </div>
     </>
   );
