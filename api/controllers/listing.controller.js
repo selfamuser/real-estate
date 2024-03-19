51bdc03040e57bc6b0ce20177ad2e5759f3318d7
Utkarsh Shukla <utkshukla9@gmail.com>
2024-03-19 15:35:12 +0530

    Update listing api route and controller created

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,5 @@
 import Listing from '../models/listing.model.js';
-import {errorHandler} from '../utils/Error.js';
+import { errorHandler } from '../utils/Error.js';
 
 export const createListing = async (req, res, next) => {
 
@@ -32,5 +32,32 @@ export const deleteListing = async (req, res, next) => {
       next(error);
     }
   };
-  
 
+
+export const updateListing = async(req, res, next) => {
+  
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+      return next(errorHandler(404,"Listing not found"));
+    }
+
+    if (req.user.id !== listing.userRef) {
+      return next(errorHandler(401, "You do not have permission to edit this listing"));
+    }
+    
+    try {
+      
+      const updatedListing = await  Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+      );
+        
+      res.status(200).json(updatedListing);
+
+    } catch (error) {
+      next(error)
+    }
+  
+}
